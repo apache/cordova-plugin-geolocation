89c4c809cb488848cecf11ab3e93e04bc941cf50
Michal Mocny <mmocny@gmail.com>
2014-04-24 15:54:36 -0400

    register tests using new style

--- test/tests.js
+++ test/tests.js
@@ -1,27 +1,6 @@
-/*
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- *
-*/
-exports.init = function() {
+// Eventually, require cdvtest plugin, for now its global
 
-    eval(require('org.apache.cordova.test-framework.test').injectJasmineInterface(this, 'this'));
-
+registerAutoTests('org.apache.cordova.geolocation', function() {
     var fail = function (done) {
         expect(true).toBe(false);
         done();
@@ -135,4 +114,4 @@ exports.init = function() {
 
     });
 
-};
+});
